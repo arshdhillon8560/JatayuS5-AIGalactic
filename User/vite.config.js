f9fdba9f6a894f758cdf6b3f3e8faa963685eae2
arshdhillon8560 <arshdhillon8560@gmail.com>
2026-05-15 01:18:59 +0530

    changes in user

--- User/vite.config.js
+++ User/vite.config.js
@@ -3,11 +3,26 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  
+  base: '/',
+
   server: {
     proxy: {
-      '/auth': 'http://localhost:5000',
-      '/application': 'http://localhost:5000',
-      '/kyc': 'http://localhost:5000',
+      '/auth': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/application': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/kyc': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
     }
   }
 })
