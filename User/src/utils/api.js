4a4f67e9340e8075294c2497fd328b45864eb623
arshdhillon8560 <arshdhillon8560@gmail.com>
2026-05-27 07:41:12 +0530

    Change api time

--- User/src/utils/api.js
+++ User/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: 'https://jatayu-s5-ai-galactic.vercel.app',
-  timeout: 120000,
+  timeout: 300000
 })
 
 api.interceptors.request.use(cfg => {
